d4f02e8de28a1b4064f25f9eaf6f400178be4577
otherworldindhu <80580975+otherworldindhu@users.noreply.github.com>
2023-04-08 16:56:21 +0530

    changes

--- astreamlit.py
+++ astreamlit.py
@@ -9,12 +9,22 @@ model.load_model('axgb_model.json')
 #Caching the model for faster loading
 @st.cache
 
-def predict(Year,College_Code, College_Name, Branch_Code,Branch_Name,OC,BC,BCMMBC,SC,SCA,ST):
+def predict(Year,College_Code, College_Name, Branch_Code,Branch_Name,OC,BC,BCM,MBC,SC,SCA,ST):
     
-    prediction = model.predict(pd.DataFrame([[Year,College_Code, College_Name, Branch_Code,Branch_Name,OC,BC,BCMMBC,SC,SCA,ST]], columns=['Year','College_Code', 'College_Name', 'Branch_Code','Branch_Name','OC','BC','BCM','MBC','SC','SCA','ST']))
+    prediction = model.predict(pd.DataFrame([[Year,College_Code, College_Name, Branch_Code,Branch_Name,OC,BC,BCM,MBC,SC,SCA,ST]], columns=['Year','College_Code', 'College_Name', 'Branch_Code','Branch_Name','OC','BC','BCM','MBC','SC','SCA','ST']))
     return prediction
 
 
 st.title('TNEA Predictor')
-st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
-st.header('Enter the characteristics of the diamond:')
+st.image("images\logo.png")
+st.header('Enter Your PCM marks and caste:')
+
+
+
+marks = st.number_input('PCM marks:', min_value=0, max_value=200, value=1)
+caste = st.selectbox('Caste:', ['OC','BC','BCM','MBC','SC','SCA','ST'])
+Year = st.selectbox('Year:', ['2017','2018','2019','2020','2021','2022','2023'])
+
+if st.button('Predict College'):
+    college = predict(College_Name,OC,BC,BCM,MBC,SC,SCA,ST)
+    st.success(f'The predicted College is ${college[0]}')
